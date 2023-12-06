faedbe64f7ed90e85375ccee43dfec6d3f271eab
mswed <mswed@beapot.com>
2023-12-05 22:20:14 -0500

    Step Two

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template, session
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I_Hhave_No_Secrets'
 boggle_game = Boggle()
+
+
+@app.route('/')
+def home():
+    session['board'] = boggle_game.make_board()
+
+    return render_template('index.html')
